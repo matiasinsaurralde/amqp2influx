eacf60a6077de5ec67896108467f9e4f3dc3ca6b
Matias Insaurralde <matias@insaurral.de>
2016-03-13 06:03:49 -0300

    Fixing

--- test/amqp2influx.js
+++ test/amqp2influx.js
@@ -1,6 +1,10 @@
+var amqp = require( 'amqplib' )
+
 describe( 'AMQP2Influx object', function() {
 
-  var AMQP2Influx
+  var AMQP2Influx, amqp2influx
+
+  this.timeout( 15000 )
 
   beforeEach( function() {
     AMQP2Influx = require( '../amqp2influx' )
@@ -10,9 +14,27 @@ describe( 'AMQP2Influx object', function() {
     AMQP2Influx.should.be.a( 'function' )
   })
 
-  it( 'should be able to be initialized', function() {
-    var amqp2influx = new AMQP2Influx()
-    amqp2influx.should.be.an.instanceof( AMQP2Influx )
+  it( 'should be initialized & process a message', function( done ) {
+
+    amqp2influx = new AMQP2Influx( function( message, influx, callback ) {
+      if( message.event === 'test' ) {
+        done()
+        callback()
+      }
+    })
+
+    amqp.connect( process.env.AMQP_URL ).then( function( connection ) {
+      connection.createChannel().then( function( channel ) {
+
+        var queue = channel.assertQueue( process.env.AMQP_QUEUE_NAME, { durable: true } ),
+            messageObject = { event: 'test' },
+            message = JSON.stringify( messageObject )
+
+        queue.then( function() {
+          channel.sendToQueue( process.env.AMQP_QUEUE_NAME, new Buffer( message ), { deliveryMode: true } )
+        })
+      })
+    })
   })
 
 })
