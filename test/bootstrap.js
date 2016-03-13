9b9469bda090a1e27b8007537b07a5fec8fee1e4
Matias Insaurralde <matias@insaurral.de>
2016-03-13 06:35:49 -0300

    Ugly fix, it seems Winston doesn't like AMQP URLs without credentials (like the one Wercker provides)

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -6,6 +6,6 @@ chai.should()
 
 process.env.NODE_ENV = 'test'
 // for compatibility with wercker:
-process.env.AMQP_URL = process.env.AMQP_URL || process.env.RABBITMQ_PORT_5672_TCP.replace( 'tcp', 'amqp' )
+process.env.AMQP_URL = process.env.AMQP_URL || process.env.RABBITMQ_PORT_5672_TCP.replace( 'tcp://', 'amqp://guest:guest@' )
 process.env.AMQP_QUEUE_NAME = process.env.AMQP_QUEUE_NAME || 'testQueue'
 
