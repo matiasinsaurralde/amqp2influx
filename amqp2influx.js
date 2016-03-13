1e08bbc91cb91ec06422eadca3fa99884332ee18
Matias Insaurralde <matias@insaurral.de>
2016-03-13 05:44:22 -0300

    Callback hell (?)

--- amqp2influx.js
+++ amqp2influx.js
@@ -1,21 +1,29 @@
 var amqp = require( 'amqplib' )
 
-var AMQP2Influx = function() {
+var AMQP2Influx = function( processMessage, initCallback ) {
 
-  var amqp2influx = this
+  var amqp2influx = this,
+      processMessage = processMessage || amqp2influx.processMessage
 
   amqp.connect( process.env.AMQP_URL ).then( function( connection ) {
+    if( initCallback ) {
+      initCallback()
+    }
+
+    console.log('123', processMessage )
+
     connection.createChannel().then( function( channel ) {
+      amqp2influx.channel = channel
       var queue = channel.assertQueue( process.env.AMQP_QUEUE_NAME, { durable: true } )
       queue.then( function() {
-        channel.consume( process.env.AMQP_QUEUE_NAME, amqp2influx.processMessage )
+        channel.consume( process.env.AMQP_QUEUE_NAME, function(envelope) {
+          console.log('123', processMessage )
+          // processMessage( envelope, channel )
+        }, { noAck: false } )
       })
     })
   })
-}
 
-AMQP2Influx.prototype.processMessage = function() {
-  console.log( 'process message!' )
 }
 
 module.exports = AMQP2Influx
