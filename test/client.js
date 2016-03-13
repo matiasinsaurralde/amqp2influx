ccdbee3089dc72b45ee6cd469fe2d223fa160eb9
Matias Insaurralde <matias@insaurral.de>
2016-03-13 06:17:54 -0300

    Preparing Winston

--- test/client.js
+++ test/client.js
@@ -1,6 +1,22 @@
+var winston = require('winston')
+
 describe( 'AMQP2Influx-Winston integration', function() {
 
-  var AMQP2Influx
+  var AMQP2Influx,
+      winston = require( 'winston' )
+
+  var AmqpWinston = require( 'amqp-winston' )
+
+  winston.loggers.add( 'worker', {
+    transports: [
+      new AmqpWinston({
+        name: 'worker', level: 'silly', host: process.env.AMQP_URL, exchange: 'log', routingKey: 'logRoutingKey'
+      })
+    ]
+  })
+
+  var logger =  winston.loggers.get( 'worker' )
+  logger.silly( 'test' )
 
   beforeEach( function() {
     AMQP2Influx = new require( '../amqp2influx' )()
