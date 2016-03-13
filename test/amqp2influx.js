0f63d3f6f022fa21ddb07338b3f8bbe6561dfd24
Matias Insaurralde <matias@insaurral.de>
2016-03-13 04:52:24 -0300

    Removing bootstrap params from individual test

--- test/amqp2influx.js
+++ test/amqp2influx.js
@@ -1,10 +1,3 @@
-var dotenv = require('dotenv')
-    dotenv.load()
-
-process.env.NODE_ENV = 'test'
-// for compatibility with wercker:
-process.env.AMQP_URL = process.env.AMQP_URL || process.env.RABBITMQ_PORT_5672_TCP.replace( 'tcp', 'amqp' )
-
 var chai = require( 'chai' )
 chai.should()
 
