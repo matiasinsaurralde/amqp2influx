19359fc370ba52ad0ce6f43a79f3101557f2c652
Matias Insaurralde <matias@insaurral.de>
2016-03-13 04:52:02 -0300

    Bootstrap mocha

---
+++ test/bootstrap.js
@@ -0,0 +1,7 @@
+var dotenv = require('dotenv')
+    dotenv.load()
+
+process.env.NODE_ENV = 'test'
+// for compatibility with wercker:
+process.env.AMQP_URL = process.env.AMQP_URL || process.env.RABBITMQ_PORT_5672_TCP.replace( 'tcp', 'amqp' )
+
