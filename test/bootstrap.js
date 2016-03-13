e7e8776e4a997c2c8c3bc5c728a4d0da1a838509
Matias Insaurralde <matias@insaurral.de>
2016-03-13 06:04:11 -0300

    Add queue name to mocha bootstrap

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -1,9 +1,11 @@
-var dotenv = require('dotenv')
-    dotenv.load()
+var dotenv = require('dotenv'),
+    chai = require( 'chai' )
+
+dotenv.load()
+chai.should()
 
 process.env.NODE_ENV = 'test'
 // for compatibility with wercker:
 process.env.AMQP_URL = process.env.AMQP_URL || process.env.RABBITMQ_PORT_5672_TCP.replace( 'tcp', 'amqp' )
+process.env.AMQP_QUEUE_NAME = process.env.AMQP_QUEUE_NAME || 'testQueue'
 
-var chai = require( 'chai' )
-    chai.should()
